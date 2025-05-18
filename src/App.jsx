a7955fd93c9226cf6ab24ccf8f4f6af14fe920b3
Carlomadella <madella871@gmail.com>
2025-05-18 22:51:00 +0200

    [UPD] completato esercizio

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,12 @@ import axios from "axios";
 function App() {
 
   // definizione variabile di stato per le attrici
-  const {actress, setActress} = useState ([]);
+  const [actresses, setActresses] = useState ([]);
 
   // definizione della funzione che effettua la chiamata axios
   const getActress = () => {
     axios.get('https://lanciweb.github.io/demo/api/actresses/').then((resp) =>{
-      console.log(resp.data)
+      setActresses(resp.data)
     })
   }
 
@@ -26,8 +26,32 @@ function App() {
         </div>
       </div>
       <div className="container">
-        <div className="row">
+        <div className="row mt-4">
           <div className="col-12"></div>
+        </div>
+        <div className="row">
+          {actresses.map((actress) => {
+            return(
+              <>
+                <div className="col-12 col-md-6 col-lg-3">
+                  <div>
+                    <div className="card-image">
+                      <img src={actress.image} className="img-fluid" />
+                    </div>
+                    <div className="card-info bg-primary">
+                      <h3>{actress.name}</h3>
+                      <p><em> Birth year:{actress.birth_year}</em></p>
+                      <br />
+                      Nationality: {actress.nationality}
+                      <br />
+                      <p>{actress.biography}</p>
+                      <p>{actress.awards}</p>
+                    </div>
+                  </div>
+                </div>
+              </>
+            )
+          })}
         </div>
       </div>
     </>
