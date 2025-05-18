9817fed4a5aab4792fe378f769bacec06367591c
Carlomadella <madella871@gmail.com>
2025-05-18 22:03:31 +0200

    [FEAT] creati file necessari

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function App() {
+  return(
+    <>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-12">
+            <h1 className="text-white">Lista attrici</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App;
