28d5f64382609a94d7f11220e6dd5b0a15777c2b
Carlomadella <madella871@gmail.com>
2025-05-18 22:12:23 +0200

    [UPD] creata struttura progetto

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,16 @@ import React from "react";
 function App() {
   return(
     <>
-      <div className="container-fluid">
+      <div className="container-fluid bg-primary">
         <div className="row">
           <div className="col-12">
             <h1 className="text-white">Lista attrici</h1>
           </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-12"></div>
         </div>
       </div>
     </>
